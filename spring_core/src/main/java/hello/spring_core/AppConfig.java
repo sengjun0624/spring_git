e2c4866b65c26561ef97c43a075ec7baf156ce04
sengjun0624 <sengjun0624@gmail.com>
2023-10-07 18:27:35 +0900

    modify version by spring

--- spring_core/src/main/java/hello/spring_core/AppConfig.java
+++ spring_core/src/main/java/hello/spring_core/AppConfig.java
@@ -9,21 +9,27 @@ import hello.spring_core.member.MemberServiceImpl;
 import hello.spring_core.member.MemoryMemberRepository;
 import hello.spring_core.order.OrderService;
 import hello.spring_core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
+    // Bean annotaion을 사용하면 스프링 컨테이너에 등록이된다.
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
--- spring_core/src/main/java/hello/spring_core/MemberApp.java
+++ spring_core/src/main/java/hello/spring_core/MemberApp.java
@@ -1,11 +1,17 @@
 package hello.spring_core;
 
 import hello.spring_core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //AppConfig에 있는 설정 정보를 스프링 컨테이너에 객체를 넣어 관리해줌.
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
